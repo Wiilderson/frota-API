5727cfa4c871eed9af30e6514d909b1e600de400
wilderson <wilderson@uitecnologia.com.br>
2024-07-12 16:14:00 -0300

    removendo pasta

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('../config/database');
+const sequelize = require('./config/database');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/caminhoes', require('../routes/caminhoes'));
-app.use('/motoristas', require('../routes/motorista'));
-app.use('/entregas', require('../routes/entregas'));
+app.use('/caminhoes', require('./routes/caminhoes'));
+app.use('/motoristas', require('./routes/motorista'));
+app.use('/entregas', require('./routes/entregas'));
 
 sequelize.sync().then(() => {
     app.listen(port, () => {
