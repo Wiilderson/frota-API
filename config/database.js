3380d32d2246da6572d3b69652b61d100fa6c715
wilderson <wilderson@uitecnologia.com.br>
2024-07-12 16:52:11 -0300

    alteracao database e git

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT,
-// });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+});
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {})
+// const sequelize = new Sequelize(process.env.POSTGRES_URL, {})
 
 module.exports = sequelize;
